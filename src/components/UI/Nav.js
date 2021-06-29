3df261931d05fcc7c3d0e20047c604b8b8387d68
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-28 19:06:33 -0500

    adds nav

--- src/components/UI/Nav.js
+++ src/components/UI/Nav.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Nav = () => {
     return (
         <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <a className="navbar-brand" href="/">Pylot</a>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/posts">Posts</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
