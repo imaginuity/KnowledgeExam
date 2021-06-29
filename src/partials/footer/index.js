1ae496ac3c2dd5262f83f32523f5f305ebbce5b8
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-06-29 01:28:35 -0500

    updates footer link colors

--- src/partials/footer/index.js
+++ src/partials/footer/index.js
@@ -10,14 +10,14 @@ const LoadFooter = (props) => {
       <footer className="text-muted py-5">
         <div className="container">
           <p className="float-end mb-1">
-            <a href="#">Back to top</a>
+            <a className="link-secondary" href="#">Back to top</a>
           </p>
           <p className="mb-1">
             Copyright © {new Date().getFullYear()} Pylot. All Rights Reserved.
           </p>
           <p className="mb-0">
-            New to Pylot? <a href="/">Visit the homepage</a> or read our{" "}
-            <a href="#">getting started guide</a>.
+            New to Pylot? <a className="link-secondary" href="/">Visit the homepage</a> or read our{" "}
+            <a className="link-secondary" href="#">getting started guide</a>.
           </p>
         </div>
       </footer>
